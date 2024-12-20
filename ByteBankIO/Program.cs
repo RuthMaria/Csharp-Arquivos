a5829017601e50d8004fcee1d94ee60d879247d1
RuthMaria <ruth-15rms@hotmail.com>
2024-12-20 17:58:08 -0300

    feat: obtem dados dos console e armazena no arquivo

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -18,8 +18,7 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        EscritaBinaria();
-        LeituraBinaria();
+        UsarStreamDeEntrada();
         Console.ReadLine();
     }
 }
